d7e2125f447f62e63accfca7ac80c1f7aaf5b211
windsor <c3fm38@gmail.com>
2023-06-01 22:34:02 +0800

    create project

---
+++ src/test/java/com/windsor/springsecuritypractice/SpringSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windsor.springsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
