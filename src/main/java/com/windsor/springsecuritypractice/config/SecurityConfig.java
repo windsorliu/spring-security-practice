ec3486211c29527fdd39074677af0ca77d3d912d
windsor <c3fm38@gmail.com>
2023-06-03 00:01:07 +0800

    update

---
+++ src/main/java/com/windsor/springsecuritypractice/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.windsor.springsecuritypractice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
+
